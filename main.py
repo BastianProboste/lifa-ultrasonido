216352aeb5a14f2527aefb85c901ba4847e52441
BastianProboste <bastian.proboste@cloud.uautonoma.cl>
2024-10-29 01:41:48 -0300

    Web

---
+++ main.py
@@ -0,0 +1,14 @@
+def bienvenida():
+    """
+    Función de bienvenida al proyecto Lifa Ultrasonido.
+    """
+    print("*************************************")
+    print("*    Bienvenido al proyecto Lifa    *")
+    print("*       Ultrasonido & Tracción      *")
+    print("*************************************")
+    print("\nEste sistema permitirá medir cambios microestructurales\n"
+          "en materiales sometidos a tracción mediante tecnología\n"
+          "de ultrasonido y análisis avanzado.\n")
+
+if __name__ == '__main__':
+    bienvenida()
