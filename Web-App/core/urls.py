e925c208f1bc4c7f77bc0b2ad2aa2c773059e72e
BastianProboste <bastian.proboste@cloud.uautonoma.cl>
2024-10-29 01:46:28 -0300

    Web

--- Web-App/core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-
-urlpatterns = [
-    path('', home, name='home'),  
-    path('module/', module, name='module'), 
-    path('login/', CustomLoginView.as_view(), name='login'),
-]
